4787927edbb3be4f02066f001ad71246bf29925d
Kim won tak <kimwontak@soonjeonggame.com>
2014-08-07 19:10:34 +0900

    Champion, Item prefab 추가.

---
+++ Assets/DynamicScroll/Example/Scripts/Champion.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Champion : MonoBehaviour
+{
+	public UISprite spriteIcon;
+	public UILabel labelName;
+}
---
+++ Assets/DynamicScroll/Example/Scripts/Item.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+	public UISprite spriteIcon;
+	public UILabel labelName;
+}
