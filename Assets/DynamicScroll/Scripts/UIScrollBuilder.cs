481652f62ad216a63e5c5e4af7bb46b042131fec
Kim won tak <kimwontak@soonjeonggame.com>
2014-08-14 19:27:03 +0900

    UIScrollBuilder 기능 개선. 	- Pivot 기능 추가. 	- Padding 기능 추가. (미구현) 	- Reposition 함수 개선 : UIScrollView.contentPivot을 기준으로 UIScrollSection을 배치할 수 있도록 변경.
    예제 적용.

--- Assets/DynamicScroll/Scripts/UIScrollBuilder.cs
+++ Assets/DynamicScroll/Scripts/UIScrollBuilder.cs
@@ -4,15 +4,26 @@ using System.Collections.Generic;
 
 public class UIScrollBuilder : MonoBehaviour
 {
-	public UIScrollView scrollView;
+	public enum Pivot
+	{
+		Left,
+		Center,
+		Right,
+	}
 
+	public UIPanel scrollPanel;
+	public Pivot pivot = Pivot.Center;
+
+	public float padding = 0f;
+	public int paddingUnitCount = 0;
+	
 	public List<UIScrollSection> listScrollSections = new List<UIScrollSection>();
 
 	void Start()
 	{
 		Reposition();
 	}
-
+	
 	public void AddScrollSection(UIScrollSection section)
 	{
 		section.transform.parent = transform;
@@ -26,20 +37,61 @@ public class UIScrollBuilder : MonoBehaviour
 
 	public void Reposition()
 	{
-		float pos = 0f;
-		
+		UIScrollView scrollView = scrollPanel.GetComponent<UIScrollView>();
+		Vector4 clipRegion = scrollPanel.baseClipRegion;
+		Vector2 pivotOffset = NGUIMath.GetPivotOffset(scrollView.contentPivot);
+		Vector2 clipOffset = new Vector3(clipRegion.x + clipRegion.z * (pivotOffset.x - 0.5f),
+		                                 clipRegion.y + clipRegion.w * (pivotOffset.y - 0.5f));
+
+		float cachedPos = 0f;
 		foreach (UIScrollSection section in listScrollSections)
 		{
-			if (scrollView.movement == UIScrollView.Movement.Horizontal)
+			Vector2 offset = new Vector3(clipOffset.x - section.Bounds.size.x * pivotOffset.x, 
+			                             clipOffset.y + section.Bounds.size.y * (1f - pivotOffset.y));
+
+ 			if (scrollView.movement == UIScrollView.Movement.Horizontal)
 			{
-				section.transform.localPosition = new Vector3(pos - section.contentWidth / 2f, section.contentHeight / 2f, 0f);
-				pos += section.Bounds.size.x;
+				section.transform.localPosition = new Vector3(cachedPos + offset.x, offset.y, 0f);
+				cachedPos += section.Bounds.size.x;
 			}
 			else
 			{
-				section.transform.localPosition = new Vector3(-(section.contentWidth / 2f), section.contentHeight / 2f - pos, 0f);
-				pos += section.Bounds.size.y;
+				section.transform.localPosition = new Vector3(offset.x, offset.y - cachedPos, 0f);
+				cachedPos += section.Bounds.size.y;
 			}
+		}
+	}
+
+	public Bounds Bounds
+	{
+		get
+		{
+			UIScrollView scrollView = scrollPanel.GetComponent<UIScrollView>();
+
+			float maxX = 0f;
+			float maxY = 0f;
+
+			for (int i=0; i<listScrollSections.Count; i++)
+			{
+				UIScrollSection section = listScrollSections[i];
+
+				if (scrollView.movement == UIScrollView.Movement.Horizontal)
+				{
+					maxX += (paddingUnitCount > 0 && i % paddingUnitCount == 0) ? padding : 0f;
+					maxX += section.Bounds.extents.x;
+					if (section.Bounds.extents.y > maxY)
+						maxY = section.Bounds.extents.y;
+				}
+				else
+				{
+					if (section.Bounds.extents.x > maxX)
+						maxX = section.Bounds.extents.x;
+					maxY += (paddingUnitCount > 0 && i % paddingUnitCount == 0) ? padding : 0f;
+					maxY += section.Bounds.extents.y;
+				}
+			}
+
+			return new Bounds(Vector3.zero, new Vector3(maxX, maxY));
 		}
 	}
 }
